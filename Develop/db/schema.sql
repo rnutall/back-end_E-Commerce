6267d6bc985ab99031ade14dc25ceb17d9c93a27
rnutall <rnutall1@gmail.com>
2023-04-14 03:17:44 -0500

    updated file

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -4,3 +4,18 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+USE courses_db;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE course_names (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL,
+  department INT,
+  FOREIGN KEY (department)
+  REFERENCES department(id)
+  ON DELETE SET NULL
+);
